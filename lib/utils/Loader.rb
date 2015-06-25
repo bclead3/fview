1d003d4af3ddd1d411e3431a961627007debd4da
Ben Leadholm <bclead3@yahoo.com>
2015-06-25 10:12:38 -0500

    updated .gitignore and modified Utils::Loader

--- lib/utils/Loader.rb
+++ lib/utils/Loader.rb
@@ -3,7 +3,12 @@ module Utils
         def initialize
             arr = Parsers::ProviderParser.process
             arr.each do |name, full_name, email_extension|
-                provider = Provider
+                puts "#{name} #{full_name}\t#{email_extension}"
+                provider = Provider.find_or_create_by(name: name)
+                provider.full_name = full_name
+                provider.email = email_extension
+                provider.save!
+                puts provider.inspect
             end
         end
     end
